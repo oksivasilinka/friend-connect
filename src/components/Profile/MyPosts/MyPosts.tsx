3008d1a340699e074dd9e3b76886fbd533b106d3
Oksana <oksana.s.kovalchuk@gmail.com>
2023-03-29 22:02:34 +0300

    unit 16 props

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -2,20 +2,18 @@ import React from "react";
 import classes from './MyPosts.module.css';
 import Post from "./MyPost/Post";
 
-function MyPosts () {
+function MyPosts() {
     return <div>
-            My posts
-            <div>
-                New post
-            </div>
-            <div className={classes.posts}>
-                <Post/>
-                <Post/>
-                <Post/>
-                <Post/>
-                <Post/>
-            </div>
+        My posts
+        <div>
+            New post
         </div>
+        <div className={classes.posts}>
+            <Post message="it's my first post" like={10}/>
+            <Post message="it's my second post" like={20}/>
+            <Post message="hello, friends" like={30}/>
+        </div>
+    </div>
 }
 
 export default MyPosts;
