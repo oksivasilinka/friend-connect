1f4579cba0cb30506340b382c35cccbceaf9d8dc
Oksana <oksana.s.kovalchuk@gmail.com>
2023-04-06 22:04:46 +0300

    unit 21 Dialogs

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,8 +3,23 @@ import s from "./Dialogs.module.css"
 
 const Dialogs = (props: any) => {
     return (
-        <div>
-            text
+        <div className={s.dialogs}>
+            <div className={s.dialogsItem}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Oksana
+                </div>
+                <div className={s.dialog}>
+                    Volodya
+                </div>
+                <div className={s.dialog}>
+                    Valera
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.dialog}>Hi</div>
+                <div className={s.dialog}>Hello!</div>
+                <div className={s.dialog}>How are you?</div>
+            </div>
         </div>
     )
 }
