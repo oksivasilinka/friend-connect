af761d838a8b5b450ca8a5aeabe86fef9fac95af
Oksana <oksana.s.kovalchuk@gmail.com>
2023-08-07 21:42:42 +0300

    unit 63 DAL, axios create

---
+++ src/api/api.ts
@@ -0,0 +1,39 @@
+import axios from "axios";
+
+const instance = axios.create({
+    withCredentials: true,
+    headers: {'API-KEY': 'b8ffb6e7-45c3-4a90-bff5-2d282762bc9f'},
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/'
+})
+
+export const usersAPI = {
+    getUsers: (currentPage: number, pageSize: number) => {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+            .then(res => res.data)
+    }
+}
+
+export const userProfileAPI = {
+    getUserProfile: (userId: string) => {
+        return instance.get(`profile/${userId}`)
+            .then(res => res.data)
+    }
+}
+
+export const authMeAPI = {
+    getAuthMe: () => {
+        return instance.get(`auth/me`)
+            .then(res => res.data)
+    }
+}
+
+export const followAPI = {
+    followUser: (userId: number) => {
+        return instance.post(`follow/${userId}`, {})
+            .then(res => res.data)
+    },
+    unFollowUser: (userId: number) => {
+        return instance.delete(`follow/${userId}`)
+            .then(res => res.data)
+    }
+}
