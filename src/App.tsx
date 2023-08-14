ca46347d5bece5fbc398ac8a52c9b85deeaea62d
Oksana <oksana.s.kovalchuk@gmail.com>
2023-08-14 11:24:17 +0300

    unit 68 Redirect

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ import {DialogsContainer} from "./Components/Dialogs/DialogsContainer";
 import {UsersContainer} from "./Components/Users/UsersContainer";
 import {ProfileContainer} from "./Components/Profile/ProfileContainer";
 import {HeaderContainer} from "./Components/Header/HeaderContainer";
+import {Login} from "./Components/login/Login";
 
 const App = () => {
 
@@ -25,6 +26,7 @@ const App = () => {
                     <Route path='/music' render={() => <Music/>}/>
                     <Route path='/settings' render={() => <Settings/>}/>
                     <Route path='/users' render={() => <UsersContainer />}/>
+                    <Route path='/login' render={() => <Login />}/>
                 </div>
                 <Sidebar/>
             </div>
