1bcc84fc198763cde336cd2281e9db69080b6318
Oksana <oksana.s.kovalchuk@gmail.com>
2023-08-29 10:07:14 +0300

    unit 84 useState

---
+++ src/components/Profile/MyPosts/ProfileInfo/ProfileStatusWithHooks.tsx
@@ -0,0 +1,43 @@
+import React, {ChangeEvent, useState} from "react";
+
+type ProfileStatusPropsType = {
+    status: string
+    updateStatus: (status: string) => void
+}
+
+export const ProfileStatusWithHooks = (props: ProfileStatusPropsType) => {
+
+    const [editMode, setEditMode] = useState(true)
+    const [status, setStatus] = useState(props.status)
+
+    const onDoubleClickHandler = () => {
+        setEditMode(!editMode)
+        props.updateStatus(status)
+    }
+
+    const onChangeStatus = (e: ChangeEvent<HTMLInputElement>) => {
+        setStatus(e.currentTarget.value)
+    }
+
+    return (
+        <div>
+            {editMode &&
+                <div>
+                    <h4>Статус: </h4>
+                    <span onDoubleClick={onDoubleClickHandler}>
+                        {status || 'no status'}
+                    </span>
+                </div>
+            }
+            {!editMode &&
+                <div>
+                    <input onBlur={onDoubleClickHandler}
+                           onChange={onChangeStatus}
+                           value={status}
+                           autoFocus>
+                    </input>
+                </div>
+            }
+        </div>
+    )
+}
