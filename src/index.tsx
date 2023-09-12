517fb917fef7be2e5ff355dbd83a8cf6dc7cfd5d
Oksana <oksana.s.kovalchuk@gmail.com>
2023-09-12 10:26:59 +0300

    unit 95 gh-pages

--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,17 @@ import React from 'react';
 import './index.css';
 import ReactDOM from "react-dom";
 import App from "./App";
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import {store} from "./redux/store";
 import {Provider} from "react-redux";
 
 
-    ReactDOM.render(
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
-            <Provider store={store}>
-                <App />
-            </Provider>
-        </BrowserRouter>
-        , document.getElementById('root')
-    );
+ReactDOM.render(
+    <HashRouter basename={process.env.PUBLIC_URL}>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </HashRouter>
+    , document.getElementById('root')
+);
 
