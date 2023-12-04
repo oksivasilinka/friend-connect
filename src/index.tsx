287acfaaeba4c090957a9d3f957200068bf33edb
Oksana <oksana.s.kovalchuk@gmail.com>
2023-12-04 16:27:51 +0300

    fix styles

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,13 @@ import { BrowserRouter } from 'react-router-dom'
 import { store } from 'redux/store'
 import { Provider } from 'react-redux'
 import { createRoot } from 'react-dom/client'
+import '@fontsource/source-sans-pro/200.css'
+import '@fontsource/source-sans-pro/300.css'
+import '@fontsource/source-sans-pro/400.css'
+import '@fontsource/source-sans-pro/600.css'
+import '@fontsource/source-sans-pro/700.css'
+import '@fontsource/source-sans-pro/900.css'
+import './variable.css'
 
 
 const root = createRoot(document.getElementById('root') as HTMLElement)
