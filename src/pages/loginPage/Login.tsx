ee571b66276f75c4760f29fe11ee35fdaa68df34
Oksana <oksana.s.kovalchuk@gmail.com>
2023-12-18 11:38:22 +0300

    update login page

--- src/pages/loginPage/Login.tsx
+++ src/pages/loginPage/Login.tsx
@@ -1,7 +1,8 @@
 import { useSelector } from 'react-redux'
 import { Navigate } from 'react-router-dom'
-import { LoginForm , isAuthSelector} from 'pages/loginPage/index'
+import { LoginForm, isAuthSelector } from 'pages/loginPage/index'
 import { Typography } from 'components/common'
+import s from './Login.module.css'
 
 export const Login = () => {
 
@@ -13,6 +14,14 @@ export const Login = () => {
     return (
         <section>
             <Typography variant={'h2'} as={'h2'}>Login</Typography>
+            <Typography variant={'body1'} className={s.text}>
+                Enter your email and password.
+                <br />
+                If you don't have an account yet, register {' '}
+                <Typography variant={'subtitle2'} as={'a'} href={'https://social-network.samuraijs.com/'}>
+                    here
+                </Typography>
+            </Typography>
             <LoginForm />
         </section>
     )
