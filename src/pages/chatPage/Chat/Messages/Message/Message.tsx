aff5f8c847014939d7f1e416c902672993a1c5b8
Oksana <oksana.s.kovalchuk@gmail.com>
2023-12-18 11:46:42 +0300

    update chat page

--- src/pages/chatPage/Chat/Messages/Message/Message.tsx
+++ src/pages/chatPage/Chat/Messages/Message/Message.tsx
@@ -12,7 +12,7 @@ export const Message = memo(({ message }: Props) => {
     return (
         <div className={s.messageCard}>
             <img className={s.avatar} src={message.photo} alt={'avatar'} />
-            <Typography variant={'subtitle3'} className={s.name}>{message.userName}</Typography>
+            <Typography variant={'body2'} className={s.name}>{message.userName}</Typography>
             <div className={s.message}>
                 <Typography variant={'body1'}>{message.message}</Typography>
             </div>
